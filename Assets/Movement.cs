603b875b6d423d1d98382bb930e4d7ecda89b280
Jamil Jewel <jamil.jewel@jet.com>
2019-09-20 15:59:08 -0400

    base project, some assets, basic scene with player movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Transform _transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            move += Vector3.up;
+        if (Input.GetKey(KeyCode.A))
+            move += Vector3.left;
+        if (Input.GetKey(KeyCode.S))
+            move += Vector3.down;
+        if (Input.GetKey(KeyCode.D))
+            move += Vector3.right;
+
+        if (move.magnitude > 0)
+        {
+            move = move.normalized * .1f;
+            _transform.position += move;
+        }
+
+        //rotate player sprite toward mouse pointer
+        var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var vector = mouse - _transform.position;
+        //get angle against the default sprite direction
+        var angle = Vector2.SignedAngle(Vector3.right, vector);
+        _transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
