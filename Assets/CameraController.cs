cbde96bc11062998726ddfb5afaada5a90215cfb
Jamil Jewel <jamil.jewel@jet.com>
2019-09-21 13:46:00 -0400

    Added barebone camera script to follow the player, one more wall

---
+++ Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform _target;
+    Transform _self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _self = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_target != null)
+        {
+            var diff = _target.position - _self.position;
+            _self.Translate(diff.x, diff.y, 0);
+        }
+    }
+}
