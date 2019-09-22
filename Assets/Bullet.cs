e2a031ef8e10320daabbfc9f367ef034b28ca856
Mahmoud Darwish <meywd@msn.com>
2019-09-22 13:19:36 -0400

    Basic Shooting and Mouse

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed = 20;
+    public int Damage = 50;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * Speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name != "Player")
+        {
+            var mouse = collision.GetComponent<Mouse>();
+            if (mouse != null)
+            {
+                mouse.TakeDamage(Damage);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Mouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public int Health = 100;
+
+    public GameObject deathEffect;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"Mouse Hit Health:{Health}");
+
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Mouse dead");
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
